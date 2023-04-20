08d19913ce1eb83b707444381bb6d2f0366b5a10
Airat <imjobjob@gmail.com>
2023-04-21 01:06:03 +0200

    random utils; ReturnType

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 from enum import Enum
+import random
+
 
 N = 100
 Nums = dict[int, int]
+
 
 def present(nums: Nums) -> set[int]:
     '''
@@ -9,8 +12,28 @@ def present(nums: Nums) -> set[int]:
     '''
     return {k for k, v in nums.items() if v > 0}
 
+
+def randint_N() -> int:
+    return random.randint(1, N)
+
+
+def list_of_randint_N(len_: int) -> list[int]:
+    return list({randint_N() for _ in range(len_)})
+
+
+def randinterval() -> int:
+    return sorted(list_of_randint_N(2))
+
+
 class ArgType(Enum):
     ONE_INT = 'one-integer-number'
     TWO_INTS = 'two-integer-numbers'
     NONE = 'no-arguments'
     N_INTS = 'any-amount-of-ints'
+
+
+class ReturnType(Enum):
+    RANDOM_NUMS = 'list-of-random-numbers'
+    PRIME_NUM = 'one-random-prime-number'
+    DUPLICATE_LITERALS = 'duplicate-all-literals' # Lit(3, 5) -> [3, 3, 5, 5]
+    
