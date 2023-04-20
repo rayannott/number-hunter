0a9b9be0c928d887ad6e5048379b1c1f26c2effe
Airat <imjobjob@gmail.com>
2023-04-20 20:30:47 +0200

    templates for operation and deal

--- obj.py
+++ obj.py
@@ -1,3 +1,22 @@
+from conditions import Condition
+
+
 class Operation:
+    '''
+    In-game operation: numbers + condition -> numbers.
+    Main way to collect new numbers
+    '''
+    def __init__(self, cost: list[int], condition: Condition, goods: list[int]) -> None:
+        '''
+        Give numbers from 'cost' and get numbers from 'goods' if the 'condition' is true
+        '''
+        pass
+
+
+class Deal:
+    '''
+    numbers + condition -> operation
+    '''
     def __init__(self) -> None:
         pass
+
