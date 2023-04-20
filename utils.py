512039b122b329bd984c23ce2cec8d5691ec2c34
Airat <imjobjob@gmail.com>
2023-04-20 22:03:24 +0200

    ArgType, present

--- utils.py
+++ utils.py
@@ -1,1 +1,14 @@
-Nums = list[int]
+from enum import Enum
+
+N = 100
+Nums = dict[int, int]
+
+def present(nums: Nums) -> set[int]:
+    '''
+    Returns a set of present numbers (amount > 0)
+    '''
+    return {k for k, v in nums.items() if v > 0}
+
+class ArgType(Enum):
+    ONE_INT = 'one-integer-number'
+    TWO_INTS = 'two-integer-numbers'
