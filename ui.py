21b4fabb0f31074ea1127ef9f35fb693e8382d90
Airat <imjobjob@gmail.com>
2023-04-21 15:55:22 +0200

    added menu

--- ui.py
+++ ui.py
@@ -1,32 +1,62 @@
 import os
-import random
-from string import ascii_letters
 import pickle
+from datetime import datetime
 
-from utils import SAVES_DIR
+from utils import SAVES_DIR, GameInfo
 from game import Game
 from exceptions import CustomException
 
 
-class App:
-    def get_game(self):
-        inp = input('New game? (y/n) ')
-        if inp == 'y':
-            self.save_name = ''.join(random.choices(ascii_letters, k=10)) + '.pi'
-            return Game()
-        # loading
-        game_files = [file for file in os.listdir('saves') if file.endswith('.pi')]
-        for i, file in enumerate(game_files):
-            print(i, file)
-        game_ind_str = input('Choose save: ')
-        self.save_name = game_files[int(game_ind_str)] # TODO try int()
-        with open(os.path.join(SAVES_DIR, self.save_name)) as f:
-            g = pickle.load(f) # TODO: does not load
-            return g
+HELP_STR = [
+    ['help', 'print this message'],
+    ['new', 'create a new game with default settings'],
+    ['new <name>', ''],
+    ['load', 'load a list of local saves'],
+    ['load <index>', 'load a game with chosen index'],
+    ['exit', 'exit the program'],
+    ['', ''],
+]
 
-    def __init__(self) -> None:
-        self.g = self.get_game()
-        self.running = True
+
+class App:
+    def menu(self):
+        print('Menu')
+        self.running_menu = True
+        while self.running_menu:
+            inp = input(': ')
+            match inp.split():
+                case ['help']:
+                    for command, info in HELP_STR:
+                        print(f'{command:<10}\t\t{info}')
+                case ['new']:
+                    self.gi = GameInfo()
+                    self.save_name = self.gi.save_name
+                    self.g = Game(self.gi)
+                    self.run()
+                case ['new', name]:
+                    self.gi = GameInfo(name)
+                    self.save_name = self.gi.save_name
+                    self.g = Game(self.gi)
+                    self.run()
+                case ['load']:
+                    self.game_files = [file for file in os.listdir('saves') if file.endswith('.pi')]
+                    if not self.game_files:
+                        print('no saves')
+                    else:
+                        for i, file in enumerate(self.game_files):
+                            print(i, file)
+                case ['load', game_ind_str]:
+                    self.game_files = [file for file in os.listdir('saves') if file.endswith('.pi')]
+                    if not self.game_files:
+                        print('no saves')
+                    else:
+                        self.save_name = self.game_files[int(game_ind_str)]
+                        with open(os.path.join(SAVES_DIR, self.save_name), 'rb') as f:
+                            self.g: Game = pickle.load(f) # TODO: does not load
+                            self.gi = self.g.info
+                        self.run()
+                case ['exit']:
+                    self.running_menu = False
 
     def display_nums(self):
         print(', '.join(f'{num}({amount})' for num, amount in self.g.numbers.items() if amount))
@@ -42,6 +72,9 @@ class App:
             case ['exit']:
                 self.running = False
                 self.save_game()
+            case ['quit']:
+                self.running = False
+                self.running_menu = False
             case ['nums']:
                 self.display_nums()
             case ['trades']:
@@ -50,7 +83,8 @@ class App:
                 self.display_nums()
                 print()
                 self.display_trades()
-            case ['trade' | 't', trade_index, *args_str]:
+            case [trade_index, *args_str]:
+                # trade!
                 args = list(map(int, args_str))
                 try:
                     returns, gifted_trade = self.g.trade(int(trade_index), args)
@@ -59,16 +93,11 @@ class App:
                 else:
                     add = f' + new trade "{gifted_trade}".' if gifted_trade else '.'
                     print(f'You received: {returns}{add}')
-            case ['shop']:
-                print(self.g.available_deals)
-            case ['shop', 'roll']:
-                # TODO
-                pass
-            case ['shop', 'buy', buy_index_str]:
-                buy_index = int(buy_index_str)
-                # TODO
+
 
     def run(self):
+        self.running = True
+        print(f'Started game {self.gi.save_name} from {datetime.fromtimestamp(self.gi.date_created_timestamp)}')
         while not self.g.is_victory() and self.running:
             inp = input('>>> ')
             self.execute_command(inp)
