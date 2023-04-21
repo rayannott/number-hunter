6508078b302f084b4bc90c30f6865a6764ecb5d0
Airat <imjobjob@gmail.com>
2023-04-21 17:10:49 +0200

    fixed typehint

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 import random
+
 from trades import TradeM
 from utils import GameInfo, N
 from trades_library import get_random_trade
 from exceptions import EmptyTradeM, CustomException
 from utils import list_of_randint_N
+from achievements import check_achievements, Achievement
+
 
 class Game:
     def __init__(self, gi: GameInfo) -> None:
@@ -13,11 +16,12 @@ class Game:
             self.numbers[el] += 1
         self.my_trades: list[TradeM] = [get_random_trade() for _ in range(10)]
         self.available_deals = []
+        self.achievements: list[Achievement] = []
 
     def is_victory(self):
         return all(self.numbers.values())
 
-    def trade(self, chosen_trade_index: int, args: list[int]) -> list[int]:
+    def trade(self, chosen_trade_index: int, args: list[int]) -> tuple[list[int], TradeM, list[Achievement]]:
         chosen_tradem = self.my_trades[chosen_trade_index]
         chosen_trade = chosen_tradem.trade
         if chosen_tradem.amount == 0:
@@ -37,10 +41,13 @@ class Game:
             if chosen_tradem == 0:
                 # remove empty tradems
                 pass
-            gifted_trade = None if random.random() < 0.5 else get_random_trade()
+            gifted_trade = None if random.random() < 0.05 else get_random_trade()
             if gifted_trade:
                 self.my_trades.append(gifted_trade)
-            return returns, gifted_trade
+
+            just_completed_achievements = check_achievements(self.numbers)
+            self.achievements.extend(just_completed_achievements)
+            return returns, gifted_trade, just_completed_achievements
             
     def roll(self):
         # TODO
