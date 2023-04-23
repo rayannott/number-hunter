9c4fba161342149c58e7aa19c9c23a9ad2b3356c
Airat <imjobjob@gmail.com>
2023-04-23 21:56:06 +0200

    new achs

--- achievements.py
+++ achievements.py
@@ -50,7 +50,6 @@ class AllBelow50(Achievement):
     def __hash__(self) -> int:
         return 4
 
-
 class AllPowersOfTwo(Achievement):
     def __call__(self, g) -> bool:
         return all(g.numbers[2**i] for i in range(7))
@@ -92,17 +91,50 @@ class DifferentTrades100(Achievement):
     def __hash__(self) -> int:
         return 10
 
+
+class AllNumbersMoreThanOne(Achievement):
+    def __call__(self, g) -> bool:
+        return all(v >= 2 for v in g.numbers.values())
+    def __hash__(self) -> int:
+        return 11
+
+
+class GoldenMiddle(Achievement):
+    def __call__(self, g) -> bool:
+        return not (any(g.numbers[i] for i in range(20)) or any(g.numbers[i] for i in range(80, 100)))
+    def __hash__(self) -> int:
+        return 12
+
+
+class AllAchievementsBeforeVictory(Achievement):
+    def __call__(self, g) -> bool:
+        return not g.is_victory() and len(g.achievements) + 1 == len(ACHIEVEMENTS)
+    def __hash__(self) -> int:
+        return 13
+
+
+class HugeWallet(Achievement):
+    def __call__(self, g) -> bool:
+        return g.sum_of_all_numbers() > 12_345
+    def __hash__(self) -> int:
+        return 14
+
+
 ACHIEVEMENTS: list[Achievement] = [
     AllBelow10('Digital Collection', 'Collect all numbers below 10'),
     AllBelow50('Lower Half', 'Collect all numbers below 50'),
-    AllPrimes('Primer in Ntheory', 'Collect all prime numbers'),
+    AllPrimes('Prime Minister', 'Collect all prime numbers'),
     AllSquares('Square Fan', 'Collect all perfect square numbers'),
     AllPowersOfTwo('True Programmer', 'Collect all powers of two'),
-    ALotOfOneNumber('Dedication', 'Collect 10 or more of one number'),
+    ALotOfOneNumber('Dedication', 'Collect 10 or more of any number'),
     GetNumberOne('Unity', 'Get a number 1'),
     TradedFirst10('Trading Amateur', 'Trade all trades with indices below 10'),
-    TwentyZeros('A bunch of 0s', 'Collect 20 or more zeros'),
-    DifferentTrades100('Trading Expert', 'Reach trade index 99')
+    TwentyZeros('A Bunch of Nothing', 'Collect 20 or more zeros'),
+    DifferentTrades100('Trading Expert', 'Reach trade index 99'),
+    AllNumbersMoreThanOne('Second Round', 'Have at least two of each number'),
+    GoldenMiddle('Golden Middle', 'Have zero numbers below 20 and above 80'),
+    HugeWallet('A Huge Wallet', 'Let the sum of your numbers (including duplicates) exceed 12345'),
+    AllAchievementsBeforeVictory('Tough Guy!', 'Complete all achievements before winning')
 ]
 
 
